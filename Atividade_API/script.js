8ef26ec7256174f71e95116c05264eb9d7f11568
Vinicius Lima <vinilimadev@gmail.com>
2022-04-12 14:51:47 -0300

    API

--- Atividade_API/script.js
+++ Atividade_API/script.js
@@ -1,17 +1,19 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/'; // constante para facilitar uso da url da api
 
+
+//constante para fazer o get na API
 const getCats = async () => {
-	try {
-		const data = await fetch(BASE_URL);
-		const json = await data.json();
-		return json.webpurl;
-	} catch (e) {
-		console.log(e.message);
+	try { //tenta executar 
+		const data = await fetch(BASE_URL); // armazena dados obtidos da api e salva em DATA
+		const json = await data.json(); // transforma os dados de DATA no tipo JSON
+		return json.webpurl; // Pega a imagem atráves do webpurl presente no JSON
+	} catch (e) { // se der erro 
+		console.log(e.message); //traz erro no log
 	}
 };
 
-const loadImg = async () => {
-	const img = document.getElementsByTagName('img')[0];
+const loadImg = async () => { // constante para tratar imagem recebida
+	const img = document.getElementById('cat');
 	img.src = await getCats();
 };
 
