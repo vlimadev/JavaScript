462352dd6a885a86327bcf4c6b829c2505f9e789
Vinicius Lima <vinilimadev@gmail.com>
2022-04-12 10:26:20 -0300

    teste dio

---
+++ teste_dio.js
@@ -0,0 +1,15 @@
+function validaArray(arr, tamanho) {
+    try {
+      if (!arr.length || arr.length !== tamanho) throw new RangeError("O tamanho do array é inválido");
+  
+      return arr;
+    } catch(e) {
+      if (e instanceof RangeError) {
+        return console.log(e.message);
+      } else {
+        return e;
+      }
+    }
+  } 
+  
+  validaArray();
